5666d7253a3c8f2b2b53f1d1c910c1a3cebff810
a-tibi <ahmd_tibi@yahoo.com>
2023-05-10 19:36:34 +0300

    the codes

---
+++ Money.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Money_OperatorOverload
+{
+	class Money
+	{
+		private readonly int amount;
+
+		public int Amount => amount;
+
+		public Money(int value)
+		{
+			amount = value;
+		}
+
+		public static Money operator +(Money money1, Money money2)
+		{
+			var value = money1.Amount + money2.Amount;
+			return new Money(value);
+		}
+
+		public static Money operator -(Money money1, Money money2)
+		{
+			var value = money1.Amount - money2.Amount;
+			return new Money(value);
+		}
+
+		public static bool operator >(Money money1, Money money2)
+		{
+			var value = money1.Amount > money2.Amount;
+			return value;
+		}
+
+		public static bool operator <(Money money1, Money money2)
+		{
+			var value = money1.Amount > money2.Amount;
+			return value;
+		}
+
+		public static bool operator >=(Money money1, Money money2)
+		{
+			var value = money1.Amount >= money2.Amount;
+			return value;
+		}
+
+		public static bool operator <=(Money money1, Money money2)
+		{
+			var value = money1.Amount <= money2.Amount;
+			return value;
+		}
+
+		public static bool operator ==(Money money1, Money money2)
+		{
+			var value = money1.Amount > money2.Amount;
+			return value;
+		}
+
+		public static bool operator !=(Money money1, Money money2)
+		{
+			var value = money1.Amount > money2.Amount;
+			return value;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Money_OperatorOverload
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var money1 = new Money(10);
+			var money2 = new Money(20);
+
+			var money3 = money1 + money2;
+			Console.WriteLine(money3.Amount);
+
+			Console.ReadKey();
+		}
+	}
+}
